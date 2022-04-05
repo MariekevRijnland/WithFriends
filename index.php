2b2758b20a90b8072035254897ef5f70e485092d
Jimmy <1033947@mborijnland.nl>
2022-04-05 12:07:13 +0200

    35g3tg
    34g34g

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
 
 
   <body id="markers-on-the-map">
-    <b>Den Haag</b>, <b>Rotterdam</b> and <b>Leiden</b> 
     <div id="map"></div>
    
     <script type="text/javascript" src='js/demo.js'></script>
