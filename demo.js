9f1e6efc31ad5253beefeb4c6a6b513c091641ef
Jimmy <1033947@mborijnland.nl>
2022-04-05 11:13:47 +0200

    sheesh
    ongod

---
+++ demo.js
@@ -0,0 +1,54 @@
+/**
+ * Adds three markers and alters the view bounds of the map to ensure that all
+ * markers are visible
+ *
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+ function addMarkersAndSetViewBounds() {
+    // create map objects
+    var leiden = new H.map.Marker({lat:52.160114,  lng:4.497010}),
+        denhaag = new H.map.Marker({lat:52.078663, lng:4.288788}),
+        rotterdam = new H.map.Marker({lat:51.926517, lng:4.462456}),
+        group = new H.map.Group();
+  
+    // add markers to the group
+    group.addObjects([leiden, denhaag, rotterdam]);
+    map.addObject(group);
+  
+    // get geo bounding box for the group and set it to the map
+    map.getViewModel().setLookAtData({
+      bounds: group.getBoundingBox()
+    });
+  }
+  
+  /**
+   * Boilerplate map initialization code starts below:
+   */
+  
+  //Step 1: initialize communication with the platform
+  // In your own code, replace variable window.apikey with your own apikey
+  var platform = new H.service.Platform({
+
+    apikey:  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgUPyQheojN6kvYsiZFvmwc-lJKwFvIwg=initMap"
+  
+  });
+  var defaultLayers = platform.createDefaultLayers();
+  
+  //Step 2: initialize a map - this map is centered over Europe
+  // note that all the markers are in North America...
+  var map = new H.Map(document.getElementById('map'),
+    defaultLayers.vector.normal.map,{
+    center: {lat:52, lng:5},
+    zoom: 25,
+    pixelRatio: window.devicePixelRatio || 1
+  });
+  // add a resize listener to make sure that the map occupies the whole container
+  window.addEventListener('resize', () => map.getViewPort().resize());
+  
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  
+  // Now use the map as required...
+  addMarkersAndSetViewBounds(map);
--- index.php
+++ index.php
@@ -9,8 +9,17 @@
 
 ?>
 <html>
-<h1>Welko  m on W/Friends</h1>
-<section class="map">
 
-</section>
+  <body id="markers-on-the-map">
+    <b>Den Haag</b>, <b>Rotterdam</b> and <b>Leiden</b> 
+    <div id="map"></div>
+   
+    <script type="text/javascript" src='demo.js'></script>
+    <script>
+      function autoRefresh() {
+          window.location = window.location.href;
+      }
+      setInterval('autoRefresh()', 600 * 100000);
+  </script>
+  </body>
 </html>
