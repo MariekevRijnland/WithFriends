8e4fdc027d13b2807c62f8c504ca277c6560c53c
MarlydeHaard <61316126+MarlydeHaard@users.noreply.github.com>
2022-04-05 13:37:18 +0200

    implemented qqrcode in javascript

--- classes/User.php
+++ classes/User.php
@@ -142,7 +142,7 @@ class User extends DbConfig
             return $randNum;
         }
     }
-    
+
     public function checkReCaptcha($captchaResponse) {
         $siteKey = '6LfBmkMfAAAAAIgZ_NfOh0H5wyhCQX8sHfVPFq_8';
         $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $siteKey . '&response=' . $captchaResponse);
--- qr-code.php
+++ qr-code.php
@@ -1,19 +1,33 @@
 <?php
-require_once 'classes/DbConfig.php';
-require_once 'classes/Friend.php';
-require_once 'classes/User.php';
+require_once './partials/autoLoader.php';
 
-$marieke = new User();
-$max = $marieke->getUsername();
 
-$code = new Friend();
-$resultCode = $code->getCode();
-
-// $result = $code->getCode();
-
+$curUser = $user->getUserById(10);
+$content = strval($curUser->friendCode);
 ?>
-<?php
-    include "phpqrcode/qrlib.php" ;
-    $content = $resultCode;
-    QRcode::png($content) ;
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script>
+        var content = <?= $content ?>;
+        content = toString(content);
+    </script>
+    <script type="text/javascript" src="./js/qrcode.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+<div id="qrcode">
+
+</div>
+
+<script type="text/javascript">
+  new QRCode(document.getElementById("qrcode"), content);
+</script>
+</body>
+</html>
