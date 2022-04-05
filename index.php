319783a7560efa3254fc208e8430ed569c89420c
Jimmy <1033947@mborijnland.nl>
2022-04-05 13:59:59 +0200

    rrhgiu324g
    oij4fuh2

--- index.php
+++ index.php
@@ -9,17 +9,49 @@
   <link rel="stylesheet" type="text/css" href="css/styles.css" />    
 
 
-<link rel="stylesheet" type="text/css" href="../template.css" />    
+<link rel="stylesheet" type="text/css" href="../template.css" />     
 <script type="text/javascript" src='js/test-credentials.js'></script>
  <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-core.js"></script>  
    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-service.js"></script>  
    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script>   
   <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script>  </head>
 
+  <head>
+  <link rel="stylesheet" href="css/style.css">
+  <title>W/ Friends</title>
+  <title>W/ Friends</title>
+</head>
+<header>
+  <div id="brand"><a href="/"><img src="images/With_friends_logo.png" alt=""></a></div>
+  <nav>
+    <ul>
+      <li><a href="/products">Messages</a></li>
+      <li><a href="/about">Friends</a></li>
+      <li><a href="/about">Name</a></li>
+      <img class="circle" src="images/user.png" alt="">
+    </ul>
+  </nav>
+  <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
+   
+     
+     
+    <div class="bar1"></div>
+    <div class="bar2"></div>
+    <div class="bar3"></div>
+    <ul class="mobile-menu">
+      <li><a href="/products">Messages</a></li>
+      <li><a href="/about">Friends</a></li>
+      <img class="circle" src="images/user.png" alt="">
+      <li><a href="/about">Name</a></li>
+
+    </ul>
+  </div>
+  <script src="js/global.js"></script>
+</header>
 
 
   <body id="markers-on-the-map">
-    <div id="map">xd</div>
+    <div id="map"></div>
    
     <script type="text/javascript" src='js/demo.js'></script>
     <script>
@@ -29,4 +61,17 @@
       setInterval('autoRefresh()', 600 * 100000);
   </script>
   </body>
+
+  </div>
+  <div class="footer">
+      <footer>
+     <ul>
+        <li><a href="Policy">Policy</a></li>
+        <li><a href="FAQ">FAQ</a></li>
+        <li><a href="Contacts">Contacts</a></li>
+      </ul>
+    </footer>
+    </div>
+
+
 </html>
