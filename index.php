ef1f26ff1e320126e183e5cc1db220f0e3d3bc02
MarlydeHaard <61316126+MarlydeHaard@users.noreply.github.com>
2022-04-07 14:35:47 +0200

    .

--- index.php
+++ index.php
@@ -1,114 +1,91 @@
-<?php 
+<?php
 require_once 'partials/autoLoader.php';
 require_once 'classes/DbConfig.php';
 
-  
+$user->get
 
 ?>
 <html>
 
-<head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes"> 
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-<title>Zooming to a Set of Markers</title>
- <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css" />
-  <link rel="stylesheet" type="text/css" href="css/demo.css" />
-  <link rel="stylesheet" type="text/css" href="css/styles.css" />
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <title>Zooming to a Set of Markers</title>
+    <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css"/>
+    <link rel="stylesheet" type="text/css" href="css/demo.css"/>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+    <link rel="stylesheet" href="css/style.css">
 
-
-<link rel="stylesheet" type="text/css" href="../template.css" />
-<script type="text/javascript" src='js/test-credentials.js'></script>
- <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-core.js"></script> 
- <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-service.js"></script>
- <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script> 
-  <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script> </head>
-
-  <head>
-  <link rel="stylesheet" href="css/style.css">
-  <title>W/ Friends</title>
-  <title>W/ Friends</title>
+    <link rel="stylesheet" type="text/css" href="../template.css"/>
+    <script type="text/javascript" src='js/test-credentials.js'></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-core.js"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-service.js"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script>
+    <title>W/ Friends</title>
 </head>
-<header>
-  <div id="brand"><a href="/"><img src="images/With_friends_logo.png" alt=""></a></div>
-  <nav>
-    <ul>
-      <li><a href="/products">Messages</a></li>
-      <li><a href="/about">Friends</a></li>
-      <li><a href="/about">Name</a></li>
-      <img class="circle" src="images/user.png" alt="">
-    </ul>
-  </nav>
-  <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
-   
-     
-     
-    <div class="bar1"></div>
-    <div class="bar2"></div>
-    <div class="bar3"></div>
-    <ul class="mobile-menu">
-      <li><a href="/products">Messages</a></li>
-      <li><a href="/about">Friends</a></li>
-      <img class="circle" src="images/user.png" alt="">
-      <li><a href="/about">Name</a></li>
 
-    </ul>
-  </div>
-  <script src="js/global.js"></script>
-</header>
-
-
-<?php 
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "wfriends";
-  
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-     $sql =  "SELECT * FROM location";
-     $result = mysqli_query($conn, $sql);
-     if (mysqli_num_rows($result) > 0)
-      
-     $i=0;
-      while($row = mysqli_fetch_array($result)) {  
-
-      echo $row["locationID"];?> <html><br/></html><?php
-      echo $row["latitude"];?> <html><br/></html><?php
-      echo $row["longitude"];?> <html><br/></html><?php 
-      echo  "<script></script>" ?>
-      
-      
-      
-      <?php
-      }
+<?php
+require_once 'partials/header.php';
 ?>
 
 
-  <body id="markers-on-the-map">
-    <div id="map"></div>
-   
-    <script type="text/javascript" src='js/demo.js'></script>
-    <script>
-      function autoRefresh() {
-          window.location = window.location.href;
-      }
-      setInterval('autoRefresh()', 600 * 100000);
-  </script>
-  </body>
+<?php
+/*$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wfriends";
 
-  </div>
-  <div class="footer">
-      <footer>
-     <ul>
-        <li><a href="Policy">Policy</a></li>
-        <li><a href="FAQ">FAQ</a></li>
-        <li><a href="Contacts">Contacts</a></li>
-      </ul>
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM location";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0)
+
+    $i = 0;
+while ($row = mysqli_fetch_array($result)) {
+
+    echo $row["locationID"]; */?><!--
+    <html><br/></html><?php
+/*    echo $row["latitude"]; */?>
+    <html><br/></html><?php
+/*    echo $row["longitude"]; */?>
+    <html><br/></html><?php
+/*    echo "<script></script>" */?>
+
+
+    --><?php
+/*}
+*/?>
+
+
+<body id="markers-on-the-map">
+<div id="map"></div>
+
+<script type="text/javascript" src='js/demo.js'></script>
+<script>
+  function autoRefresh() {
+    window.location = window.location.href;
+  }
+
+  setInterval('autoRefresh()', 600 * 100000);
+</script>
+</body>
+
+</div>
+<div class="footer">
+    <footer>
+        <ul>
+            <li><a href="Policy">Policy</a></li>
+            <li><a href="FAQ">FAQ</a></li>
+            <li><a href="Contacts">Contacts</a></li>
+        </ul>
     </footer>
-    </div>
+</div>
 
 
 </html>
