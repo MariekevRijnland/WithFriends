63e52d25d9fe42c7673f4a656281b734906cac00
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-04-07 15:16:37 +0200

    added file

--- faq.php
+++ faq.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>W/Friends | FAQ</title>
+</head>
 <?php
-
+ require_once './partials/header.php';
 ?>
+<body>
+    <h1>FAQ</h1>
+</body>
+<?php
+ require_once './partials/footer.php';
+?>
+</html>
