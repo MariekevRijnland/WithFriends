d524c45866e458a5aeead6370220c063a05399f9
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-04-07 14:34:36 +0200

    Added comments

--- addFriend.php
+++ addFriend.php
@@ -1,17 +1,20 @@
 <?php
+    // Link to autoloader  
     include 'partials/autoLoader.php';
 
+    // If the one of the buttons is clicked It'll redirect to the said page
     if (isset($_POST['yes'])) {   
     }
     if (isset($_POST['no'])) {
-        header('Location: index.html');
+        header('Location: index.php');
     }
     if (!isset($_GET['friendCode'])) {
-        header('Location: index.html');
+        header('Location: index.php');
     }
-
+    // Picks up all users
     $users = $user->getUsers();
 
+    // Links friendCode to userID
     foreach ($users as $userIns) {
         if (isset($userIns->friendCode) && strval($userIns->friendCode) == $_GET['friendCode']) {
             $friend = $user->getUserById($userIns->userID);
@@ -21,20 +24,25 @@
 <!doctype html>
 <html lang="en">
     <head>
+        <!-- Link to style sheets -->
         <link rel="stylesheet" href="./CSS/footer.css">
         <link rel="stylesheet" href="./CSS/header.css">
         <link rel="stylesheet" href="./CSS/addFriend.css">
         <title>W/Friends | Add Friend</title>
     </head>
-    <?= require_once './partials/header.php'?>
+    <!-- Link to header -->
+    <?php require_once './partials/header.php'?>
     <body>
         <main>
             <form method="POST">
+                <!-- Shows the name of the person the friendCode is from -->
                 <h1>You want to add <?= $friend->name ?> as a friend?</h1>
+                <!-- Confirm buttons -->
                 <input class="addfriend-btn" type="submit" value="Yes" name="yes">
                 <input class="addfriend-btn" type="submit" value="No" name="no">
             </form>
         </main>
     </body>
-    <?=require_once './partials/footer.php'?>
+    <!-- Link to footer -->
+    <?php require_once './partials/footer.php'?>
 </html>
--- qr-code.php
+++ qr-code.php
@@ -1,24 +1,32 @@
 
 <?php
+// Link to autoloader
 require_once './partials/autoLoader.php';
 session_start();
+// Variable that picks up the ID of the user
 $curUser = $user->getUserById($_SESSION['userID']);
+// Variable that makes friendCode into string
 $content = strval($curUser->friendCode);
 ?>
 <!doctype html>
 <html lang="en">
   <head>
+      <!-- Link to style sheets -->
       <link rel="stylesheet" href="./CSS/qrcode.css">
       <link rel="stylesheet" href="./CSS/header.css">
       <link rel="stylesheet" href="./CSS/footer.css">
       <script>
+        // Variable that stores the friendCode of the php code
           var content = <?= $content ?>;
+        // Variable that converts the php code to a string in JavaScript
           content = toString(content);
       </script>
+      <!-- Link to JavaScript scripts -->
       <script type="text/javascript" src="./js/qrcode.js"></script>
       <script type="text/javascript" src="../js/global.js"></script>
       <title>W/Friends | QR Code</title>
   </head>
+  <!-- Link to the header -->
   <?= require_once './partials/header.php';?>
   <body>
       <p class="add">Scan the QR code to be added as friend!</p>
@@ -28,8 +36,10 @@ $content = strval($curUser->friendCode);
         </div>
       </div>
       <script type="text/javascript">
+        // Generates new QR code with link to friendCode
         new QRCode(document.getElementById("qrcode"), "38.242.233.110/addFriend.php?friendCode="+content);
       </script>
   </body>
 </html>
+<!-- Link to the footer -->
 <?= require_once './partials/footer.php';?>
