d12e81f2a0bc27ebf439f95d0f6adc812f3ccdc0
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-04-07 15:46:13 +0200

    added questions

--- faq.php
+++ faq.php
@@ -8,18 +8,44 @@
  require_once './partials/header.php';
 ?>
 <body>
-    <h1>Frequently Asked Questions</h1>
-    <div class="wrap-collabsible">
-  <input id="collapsible" class="toggle" type="checkbox">
-  <label for="collapsible" class="lbl-toggle">Can I block certain friends from seeing my location?</label>
-  <div class="collapsible-content">
-    <div class="content-inner">
-      <p>
-      The current version of the app doesn’t allow you to block friends from seeing your location. In future updates we may implement this feature. If you delete a friend, they will no longer be able to see your location.
-      </p>
-    </div>
-  </div>
+    <h1>Fequently Asked Questions</h1>
+<button type="button" class="collapsible">Will my data be sold to third parties?</button>
+<div class="content">
+  <p>No, your data will not be sold or shared to any other parties but W/Friends Inc.</p>
 </div>
+<button type="button" class="collapsible">Can I block certain friends from seeing my location?</button>
+<div class="content">
+  <p> The current version of the app doesn’t allow you to block friends from seeing your location. In future updates we may implement this feature. If you delete a friend, they will no longer be able to see your location. </p>
+</div>
+<button type="button" class="collapsible">How do I add my friends?</button>
+<div class="content">
+  <p>You can add your friends on the home page or on the friends’ page. There are many ways you can add them using WhatsApp, Facebook or copying the link to your clipboard. Send the link to your friends and they will be able to accept or decline the invite on that link.</p>
+</div>
+<button type="button" class="collapsible">Can I customize my Friend code?</button>
+<div class="content">
+  <p>No, the Friend code is a unique code generated at registration. Currently we don’t allow the Friend code to be customized.</p>
+</div>
+<button type="button" class="collapsible">How do I remove my friends?</button>
+<div class="content">
+  <p>You can remove your friends on the home page or on the friends' page. You can use the dropdown of the friend you want to remove and simply press “Remove Friend”.</p>
+</div>
+<button type="button" class="collapsible">How do I delete my account?</button>
+<div class="content">
+  <p>You can delete your account on the Account Settings Page and press the “Delete Account” button.</p>
+</div>
+<button type="button" class="collapsible">How do I add my friends?</button>
+<div class="content">
+  <p>You can add your friends on the home page or on the friends’ page. There are many ways you can add them using WhatsApp, Facebook or copying the link to your clipboard. Send the link to your friends and they will be able to accept or decline the invite on that link.</p>
+</div>
+
+
+
+
+
+
+
+
+<script src="./js/faq.js"></script>
 </body>
 <?php
  require_once './partials/footer.php';
