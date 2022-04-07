206d06cc9d93278a63ee828b98787cf86e1c873a
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-04-07 15:29:38 +0200

    FAQ update

--- faq.php
+++ faq.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+    <link rel="stylesheet" href="./CSS/faq.css">
     <title>W/Friends | FAQ</title>
 </head>
 <?php
@@ -9,6 +9,17 @@
 ?>
 <body>
     <h1>Frequently Asked Questions</h1>
+    <div class="wrap-collabsible">
+  <input id="collapsible" class="toggle" type="checkbox">
+  <label for="collapsible" class="lbl-toggle">Can I block certain friends from seeing my location?</label>
+  <div class="collapsible-content">
+    <div class="content-inner">
+      <p>
+      The current version of the app doesn’t allow you to block friends from seeing your location. In future updates we may implement this feature. If you delete a friend, they will no longer be able to see your location.
+      </p>
+    </div>
+  </div>
+</div>
 </body>
 <?php
  require_once './partials/footer.php';
