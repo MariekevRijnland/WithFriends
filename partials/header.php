eff1024d594d80c4aafed23bf2faa12eafa796ea
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-04-07 12:19:56 +0200

    added alt text to images

--- partials/header.php
+++ partials/header.php
@@ -2,16 +2,15 @@
 <head>
     <link rel="stylesheet" href="css/style.css">
     <title>W/ Friends</title>
-    <title>W/ Friends</title>
 </head>
 <header>
-    <div id="brand"><a href="/"><img src="images/With_friends_logo.png" alt=""></a></div>
+    <div id="brand"><a href="/"><img src="images/With_friends_logo.png" alt="With Friends Logo"></a></div>
     <nav>
       <ul>
         <li><a href="/products">Messages</a></li>
         <li><a href="/about">Friends</a></li>
         <li><a href="/about">Name</a></li>
-        <img class="circle" src="images/user.png" alt="">
+        <img class="circle" src="images/user.png" alt="Profile picture">
       </ul>
     </nav>
     <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
@@ -24,7 +23,7 @@
       <ul class="mobile-menu">
         <li><a href="/products">Messages</a></li>
         <li><a href="/about">Friends</a></li>
-        <img class="circle" src="images/user.png" alt="">
+        <img class="circle" src="images/user.png" alt="User profile picture">
         <li><a href="/about">Name</a></li>
 
       </ul>
